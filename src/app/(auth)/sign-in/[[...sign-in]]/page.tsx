e4b4d5325710b18259dd0d374d8b38919fc3f362
TaherMustansir1929 <eren.jeager1929101@gmail.com>
2025-12-08 21:54:57 +0500

    Fix: sign-in page UI bugs

--- src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,12 +1,14 @@
 import { cn } from "@/lib/utils";
 import { SignIn } from "@clerk/nextjs";
+import Image from "next/image";
 
 export default function Page() {
   return (
     <div className={cn("flex flex-col items-center justify-center py-24 px-4 bg-muted/10")}>
       <div className="mb-8 text-center">
+        <Image src={"/icon.svg"} alt="logo" width={30} height={30} className="inline-block"/>
         <h1 className="text-3xl font-bold">Linear</h1>
-        <p className="text-muted-foreground">Prep for exams together.</p>
+        <p className="text-muted-foreground">Prep for exams {"<together/>"}</p>
       </div>
       <SignIn />
     </div>
